5e6d63271a321b709f827b82ccba79d012874d84
Eden Tyler-Moss <contact@edmoss.me>
2018-10-31 02:26:50 +0000

    Add files via upload

--- simpleplot.sh
+++ simpleplot.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 cycles=0
 
-[ "$#" = 0 ] && echo "[ERROR]: No arguments were supplied. Try -h for a list of acceptable syntax." && exit
-
 while getopts ":f:sc:r:Sh" opt; do
-	case $opt in
+	case "$opt" in
 		s)
 			silent=1
 			;;
 		h)
-			echo "Usage: simpleplot.sh [OPTION] [TARGET]"
+			echo "Usage: plotsense.sh [OPTION] [TARGET]"
 			echo " -f [ARG],	Output log to file [ARG]"
 			echo " -c [ARG],	Number of cycles, in [ARG] of cycles"
 			echo " -r [ARG],	Duration of each cycle, in [ARG] seconds (Default: 1)"
@@ -23,38 +21,36 @@ while getopts ":f:sc:r:Sh" opt; do
 			filefluff=1
 			;;
 		f)
-			outputfile=$OPTARG
+			outputfile="$OPTARG"
 			;;
 		r)
-			spacetime=$OPTARG
+			spacetime="$OPTARG"
 			;;
 		c)
-			durationvar=$OPTARG
-			[[ $OPTARG =~ ^-?[0-9]+$ ]] || (echo "[ERROR]: $OPTARG Is not a integer." && exit 3)
+			durationvar="$OPTARG"
+			[[ "$OPTARG" =~ ^-?[0-9]+$ ]] || (echo "[ERROR]: "$OPTARG" Is not a integer." && exit 1)
+			[[ $? != 0 ]] && exit $?
 			;;
 		\?)
-			echo "Invalid option: -$OPTARG" >&2
+			echo "Invalid option: -"$OPTARG"" >&2
 			exit
 			;;
 		:)
-			echo "-$OPTARG Requires an argument." >&2
+			echo "-"$OPTARG" Requires an argument." >&2
 			exit
 			;;
 	esac
 done
 shift $((OPTIND - 1))
-
+[ "$#" = 0 ] && echo "[ERROR]: No arguments were supplied. Try -h for a list of acceptable syntax." && exit
 
 targetarg="$1"
-
 function measurementfield1 {
 	sensors | awk -v targetarg="$targetarg" '$0 ~ targetarg{print}' | awk -F: '//{print $2}' | awk '//{print $1}' | sed 's/+//g'
 }
-
 function awkvalue {
 	sensors | awk -v targetarg="$targetarg" '$0 ~ targetarg{print}' | awk -F: '//{print $2}' | awk '//{print $1" "$2}' | sed 's/+//g'
 }
-
 function measurementlastchar {
 	echo "$(awkvalue)" | sed -e 's/\(^.*\)\(.$\)/\2/'
 }
@@ -73,22 +69,23 @@ else
 	exit
 fi
 
-[ -z "$spacetime" ] && spacetime=1 # Sets the default time between samples
+[ -z "$spacetime" ] && spacetime=1
 [ -w "$outputfile" ] && rm "$outputfile" || ([ -e "$outputfile" ] && (echo "$outputfile" not writable. ; kill $$))
 [ -z $silent ] && (echo "Starting log:")
 
-if [ -n "$filefluff" ] ; then
-echo "# Made with simpleplot.sh" >> $outputfile
-echo "# Number of cycles: $durationvar" >> $outputfile
-echo "# Cycle duration: ${spacetime}s" >> $outputfile
-printf "\nCycles	Value\n" >> $outputfile
+if [ -n "$filefluff" ] && [ -n "$outputfile" ] ; then
+echo "# Made with plotsense.sh" >> "$outputfile"
+echo "# Target was: "$targetarg"" >> "$outputfile"
+echo "# Number of cycles: "$durationvar"" >> "$outputfile"
+echo "# Cycle duration: "${spacetime}"s" >> "$outputfile"
+printf "\nCycles	Value\n" >> "$outputfile"
 fi
 
 while true
 do
 	[ -n "$durationvar" ] && [ "$cycles" -eq "$durationvar" ] && echo "Completed final cycle." && exit
-	value=$(awkvalue) # Grab value from lm_sensors
-	[ -n "$outputfile" ] && echo "$cycles	$value" >> "$outputfile" # Append value to file
+	value=$(awkvalue)
+	[ -n "$outputfile" ] && echo "$cycles	$value" >> "$outputfile"
 	if [ -z "$silent" ] ; then
 		[ "$cycles" -eq 0 ] && echo "Cycles	Value"
 		echo "$cycles	$value"
